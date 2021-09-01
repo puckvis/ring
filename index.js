3dab9d16cf714b1316c420d8a8737354a4e064f4
puckvis <puck@puckvisser.nl>
2021-09-01 15:41:06 +0200

    commit ring

---
+++ index.js
@@ -0,0 +1,9 @@
+const toTitleCase = require("to-title-case");
+
+const title = "test title";
+
+console.log("original title: ", title);
+
+const newTitle = toTitleCase(title);
+
+console.log("new title: ", newTitle);
